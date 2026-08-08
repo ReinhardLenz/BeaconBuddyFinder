232803c64e91dbd2c9607b9c44a8434bf3624f9e
Reinhard Lenz <reinhard.lenz@konecranes.com>
2026-08-08 19:28:48 +0300

    AXP2101 added

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,26 @@
 #include "compass.h"
 #include "ReceiveBuddyInfo.h"
 #include "SendOwnInfo.h"
-#include "LED.h"
+#include <FastLED.h>
+#include <Wire.h>
+// ------------------------------------------------------------
+// T-Beam v1.2 AXP2101 I2C
+// ------------------------------------------------------------
 
+#define I2C_SDA       21
+#define I2C_SCL       22
+
+#define AXP2101_ADDR  0x34
+
+#define AXP2101_DLDO1_VOL  0x99
+#define AXP2101_DLDO_EN    0x9C
+
+
+// ------------------------------------------------------------
+// Serial configuration
+// ------------------------------------------------------------
+
+#define USB_BAUD  115200
 // --------------------
 // from BNO085 UART
 // --------------------
@@ -70,6 +88,53 @@ void fatalError(ErrorCode code)
     }
 }
 
+// ------------------------------------------------------------
+// AXP2101 helpers
+// ------------------------------------------------------------
+
+void axpWrite(uint8_t reg, uint8_t val)
+{
+    Wire.beginTransmission(AXP2101_ADDR);
+    Wire.write(reg);
+    Wire.write(val);
+    Wire.endTransmission();
+}
+
+
+uint8_t axpRead(uint8_t reg)
+{
+    Wire.beginTransmission(AXP2101_ADDR);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+
+    Wire.requestFrom((uint8_t)AXP2101_ADDR, (uint8_t)1);
+
+    return Wire.available() ? Wire.read() : 0xFF;
+}
+
+
+void enableGPSPower()
+{
+    // DLDO1 = 3.3 V
+    axpWrite(AXP2101_DLDO1_VOL, 0x1C);
+
+    delay(20);
+
+    // Enable DLDO1, preserving the other bits
+    uint8_t reg = axpRead(AXP2101_DLDO_EN);
+
+    axpWrite(AXP2101_DLDO_EN, reg | 0x01);
+
+    delay(200);
+}
+
+
+// ------------------------------------------------------------
+// NMEA line buffer
+// ------------------------------------------------------------
+
+
+
 // --------------------
 // pins for Lora
 // --------------------
@@ -115,9 +180,46 @@ static inline float wrap360(float deg) {
 }
 
 void setup() {
+
+  Serial.begin(USB_BAUD);
 // --------------------
 // start BNO085 UART
-// --------------------
+// -
+
+  // --------------------------------------------------------
+  // AXP2101
+  // --------------------------------------------------------
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  delay(50);
+
+  Wire.beginTransmission(AXP2101_ADDR);
+
+  if (Wire.endTransmission() == 0) {
+
+      Serial.println();
+      Serial.println(
+          "AXP2101 detected -> enabling GPS power..."
+      );
+
+      enableGPSPower();
+
+      Serial.println("GPS power enabled.");
+
+  } else {
+
+      Serial.println();
+      Serial.println(
+          "WARNING: AXP2101 not detected at 0x34."
+      );
+
+      Serial.println(
+          "GPS may be unpowered."
+      );
+  }
+
+-------------------
 
     Serial.begin(PC_BAUD);
     Serial2.begin(BNO_BAUD, SERIAL_8N1, PIN_BNO_RX, PIN_BNO_TX);
